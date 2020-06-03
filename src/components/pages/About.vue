f67133d0a05ea66dbdcace1c42f737d012c8139d
Satya Wikananda <satyawikananda456@gmail.com>
2020-06-03 16:53:27 +0800

    :sparkles: Add about and favorite pages

---
+++ src/components/pages/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <h1>hello world</h1>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "About"
+};
+</script>
---
+++ src/components/pages/Favorite.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <h1>Hello world</h1>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "Favorite"
+};
+</script>
