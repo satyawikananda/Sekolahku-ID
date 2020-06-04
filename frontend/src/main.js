1b31215b89865fadad818c837f1ad7af2caa1e23
Satya Wikananda <satyawikananda456@gmail.com>
2020-06-05 00:39:15 +0800

    :zap: (Update): Make base url global

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./routes/Router.js";
 import "./registerServiceWorker";
+import Axios from "axios";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+Vue.prototype.$http = Axios;
 
 new Vue({
   vuetify,
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
