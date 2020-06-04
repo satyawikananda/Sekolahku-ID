8ccd623faa4e23908df914b81837b56076c20fa9
Satya Wikananda <satyawikananda456@gmail.com>
2020-06-04 22:02:26 +0800

    :fire: Remove some code

--- api/src/data/wilayah.ts
+++ api/src/data/wilayah.ts
@@ -1,36 +1,28 @@
-import {baseUrl} from "../const"
-import axios from "axios"
+import { baseUrl } from "../const";
+import axios from "axios";
 
 export const wilayah: any = () => {
-    return new Promise( async (resolve:any, reject: any) => {
-        await axios.get(`${baseUrl}cwilayah/wilayahKabGet`)
-         .then((response: any):void => {
-            if(response.status === 200){
-                const result:any = response.data
-                // const hasil: object[] = []
-
-                // result.forEach((data: any):void => {
-                //     hasil.push({
-                //         kode_wilayah: data.kode_wilayah,
-                //         nama: data.nama,
-                //         mst_kode_wilayah: data.mst_kode_wilayah
-                //     })
-                // })
-                resolve(result)
-            }else{
-                type Message = {
-                    code: number,
-                    msg: string,
-                }
-                const error: Message = {
-                    code: response.status,
-                    msg: "Failed"
-                }
-                resolve(error)
-            }
-         })
-         .catch((error: any):void =>{
-            reject(error)
-         })
-    })
-}
+  return new Promise(async (resolve: any, reject: any) => {
+    await axios
+      .get(`${baseUrl}cwilayah/wilayahKabGet`)
+      .then((response: any): void => {
+        if (response.status === 200) {
+          const result: any = response.data;
+          resolve(result);
+        } else {
+          type Message = {
+            code: number;
+            msg: string;
+          };
+          const error: Message = {
+            code: response.status,
+            msg: "Failed",
+          };
+          resolve(error);
+        }
+      })
+      .catch((error: any): void => {
+        reject(error);
+      });
+  });
+};
