a3573bb5fc9b3545fc6b722226cce26c6ede27e1
Satya Wikananda <satyawikananda456@gmail.com>
2020-06-05 22:09:36 +0800

    :zap: Finish search page

--- frontend/src/components/pages/Search.vue
+++ frontend/src/components/pages/Search.vue
@@ -1,21 +1,42 @@
 <template>
-  <v-layout column align-center justify-center class="ma-5 pb-5">
+  <v-container grid-list-xl>
     <h3
       class="red--text text--darken-2 text-center mt-3 pa-3 title font-weight-reguler"
     >Pilih wilayah</h3>
-    <span
-      class="red--text text--darken-2 text-center pb-2 subtile-1 font-weight-reguler"
-    >Wilayah yang dicari: {{searchWilayah}}</span>
-    <v-select
-      v-model="searchWilayah"
-      menu-props="auto"
-      label="Pilih wilayah"
-      dense
-      outlined
-      full-width
-      :items="wilayah"
-    ></v-select>
-  </v-layout>
+    <v-layout row justify-space-between>
+      <v-flex md4 mx-auto>
+        <p
+          class="red--text text--darken-2 text-center pb-2 subtile-1 font-weight-reguler"
+        >Wilayah yang dicari: {{searchWilayah}}</p>
+        <v-select
+          v-model="searchWilayah"
+          menu-props="auto"
+          label="Pilih wilayah"
+          solo
+          outline
+          full-width
+          :items="wilayah"
+        ></v-select>
+      </v-flex>
+      <v-flex md4 mx-auto>
+        <p
+          class="red--text text--darken-2 text-center pb-2 subtile-1 font-weight-reguler"
+        >Tingkatan sekolah: {{tingkatanSekolah}}</p>
+        <v-select
+          v-model="tingkatanSekolah"
+          menu-props="auto"
+          label="Pilih tingkatan sekolah"
+          solo
+          outline
+          full-width
+          :items="bentuk"
+        ></v-select>
+      </v-flex>
+      <v-flex md5 mx-auto>
+        <v-btn color="primary" style="width:100%" dark large>Cari sekolah</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
 import { getWilayah } from "@/mixins/getWilayah.js";
@@ -24,7 +45,9 @@ export default {
   mixins: [getWilayah],
   data() {
     return {
-      searchWilayah: ""
+      searchWilayah: "",
+      tingkatanSekolah: "",
+      bentuk: ["SD", "SMP", "SMA", "SMK"]
     };
   }
 };
