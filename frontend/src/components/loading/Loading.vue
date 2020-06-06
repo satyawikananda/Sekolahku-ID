94d2d2cbc89bc56b6ebde847e8db1ddac7f46ceb
Satya Wikananda <satyawikananda456@gmail.com>
2020-06-07 00:31:34 +0800

    :sparkles: Add loading

---
+++ frontend/src/components/loading/Loading.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center">
+    <v-progress-circular
+      indeterminate
+      rotate="10"
+      size="120"
+      width="5"
+      color="secondary"
+      class="pa-3"
+    >
+      {{ loading }}
+    </v-progress-circular>
+  </div>
+</template>
+<script>
+export default {
+  props: ["loading"]
+};
+</script>
--- frontend/src/mixins/getSekolah.js
+++ frontend/src/mixins/getSekolah.js
@@ -4,24 +4,28 @@ export const getSekolah = {
     return {
       kode_wilayah: "",
       tingkatanSekolah: "",
+      isLoading: false,
       data: []
     };
   },
   methods: {
     async search() {
       try {
+        this.isLoading = true;
         const { data } = await this.$http.request({
           method: "GET",
           url: `${baseUrl}sekolah`,
           params: {
             bentuk: this.tingkatanSekolah,
-            kode_wilayah: this.kode_wilayah,
-          },
+            kode_wilayah: this.kode_wilayah
+          }
         });
-        this.data = data.data
+        this.data = data.data;
+        this.isLoading = false;
       } catch (err) {
+        this.isLoading = false;
         throw new err();
       }
-    },
-  },
+    }
+  }
 };
