377abb7863455649af0a54864b9bc9706cf12f9b
Satya Wikananda <satyawikananda456@gmail.com>
2020-06-07 00:30:42 +0800

    :zap: Update code API

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { Sekolah } from "./data/index"
+import { NowRequest, NowResponse } from "@now/node"
 import cors from "cors"
 
 dotenv.config()
@@ -14,7 +15,7 @@ app.get("/", (req, res): void => {
     })
 })
 
-app.get("/api/wilayah", (req, res): void => {
+app.get("/api/wilayah", (req: NowRequest, res: NowResponse): void => {
     res.setHeader("Cache-Control", "public,max-age=3600,s-maxage=30");
     setImmediate(():void => {
         try{
@@ -28,16 +29,24 @@ app.get("/api/wilayah", (req, res): void => {
     })
 })
 
-app.get("/api/sekolah", (req, res): void => {
+app.get("/api/sekolah", (req: NowRequest, res: NowResponse): void => {
     const kodeWilayah: any = req.query.kode_wilayah
     const bentuk: any = req.query.bentuk
     res.setHeader("Cache-Control", "public,max-age=3600,s-maxage=30");
     setImmediate(():void => {
         try{
-            Sekolah.sekolah(kodeWilayah, bentuk)
-             .then((data: any):void => {
-                 res.send(data)
-             })
+            if(kodeWilayah === null||  bentuk === null){
+                res.status(400).json({
+                    code: res.statusCode,
+                    success: false,
+                    message: "Query string can not be empty!"
+                })
+            }else{
+                Sekolah.sekolah(kodeWilayah, bentuk)
+                 .then((data: any):void => {
+                     res.send(data)
+                 })
+            }
         }catch(err){
             res.status(400).send("Something went wrong")
         }
