83e72d29886a961a12607eb1c63db700b385af27
Satya Wikananda <satyawikananda456@gmail.com>
2020-06-07 00:47:21 +0800

    :sparkles: Add search sekolah feature

---
+++ frontend/src/components/data/Data.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <v-expansion-panel-header>{{ sekolah }}</v-expansion-panel-header>
+    <v-expansion-panel-content>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>NPSN</v-list-item-title>
+          <v-list-item-subtitle>{{ npsn }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>Bentuk sekolah</v-list-item-title>
+          <v-list-item-subtitle>{{ bentuk }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>Status sekolah</v-list-item-title>
+          <v-list-item-subtitle>{{ status }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>Alamat sekolah</v-list-item-title>
+          <v-list-item-subtitle>{{ alamat }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>Provinsi sekolah</v-list-item-title>
+          <v-list-item-subtitle>{{ provinsi }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>Kabupaten sekolah</v-list-item-title>
+          <v-list-item-subtitle>{{ kab }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>Kecamatan sekolah</v-list-item-title>
+          <v-list-item-subtitle>{{ kec }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-btn color="secondary" class="ma-2 white--text">
+        Pin
+        <v-icon right dark>mdi-heart-outline</v-icon>
+      </v-btn>
+      <v-btn color="secondary" class="ma-2 white--text">
+        Lihat lokasi
+        <v-icon right dark>mdi-map</v-icon>
+      </v-btn>
+    </v-expansion-panel-content>
+  </div>
+</template>
+<script>
+export default {
+  props: [
+    "sekolah",
+    "npsn",
+    "bentuk",
+    "status",
+    "alamat",
+    "provinsi",
+    "kab",
+    "kec"
+  ],
+  data() {
+    return {
+      like: false
+    };
+  }
+};
+</script>
--- frontend/src/components/pages/Search.vue
+++ frontend/src/components/pages/Search.vue
@@ -2,12 +2,16 @@
   <v-container grid-list-xl>
     <h3
       class="red--text text--darken-2 text-center mt-3 pa-3 title font-weight-reguler"
-    >Pilih wilayah</h3>
+    >
+      Pilih wilayah
+    </h3>
     <v-layout row justify-space-between>
       <v-flex md4 mx-auto>
         <p
           class="red--text text--darken-2 text-center pb-2 subtile-1 font-weight-reguler"
-        >Wilayah yang dicari</p>
+        >
+          Wilayah yang dicari
+        </p>
         <v-select
           menu-props="auto"
           label="Pilih wilayah"
@@ -23,7 +27,9 @@
       <v-flex md4 mx-auto>
         <p
           class="red--text text--darken-2 text-center pb-2 subtile-1 font-weight-reguler"
-        >Tingkatan sekolah</p>
+        >
+          Tingkatan sekolah
+        </p>
         <v-select
           v-model="tingkatanSekolah"
           menu-props="auto"
@@ -35,7 +41,46 @@
         ></v-select>
       </v-flex>
       <v-flex md5 mx-auto>
-        <v-btn color="primary" @click.prevent="search" style="width:100%" dark large>Cari sekolah</v-btn>
+        <v-btn
+          color="primary"
+          @click.prevent="search"
+          style="width:100%"
+          dark
+          large
+          >Cari sekolah</v-btn
+        >
+      </v-flex>
+    </v-layout>
+    <div v-if="isLoading">
+      <loading loading="Mohon tunggu" />
+    </div>
+    <div v-if="data.length == 0">
+      <v-layout row justify-space-between>
+        <v-flex md8 mx-auto>
+          <h3
+            class="red--text text--darken-2 text-center mb-3 pa-3 title font-weight-reguler"
+          >
+            Data tidak di temukan
+          </h3>
+        </v-flex>
+      </v-layout>
+    </div>
+    <v-layout row justify-space-between v-else>
+      <v-flex md8 mx-auto>
+        <v-expansion-panels focusable>
+          <v-expansion-panel v-for="(data, index) in data" :key="index">
+            <sekolah
+              :sekolah="data.sekolah"
+              :npsn="data.npsn"
+              :bentuk="data.bentuk"
+              :status="data.status == 'n' ? 'Negeri' : 'Swasta'"
+              :alamat="data.alamat_jalan"
+              :provinsi="data.propinsi"
+              :kab="data.kabupaten_kota"
+              :kec="data.kecamatan"
+            />
+          </v-expansion-panel>
+        </v-expansion-panels>
       </v-flex>
     </v-layout>
   </v-container>
@@ -43,6 +88,8 @@
 <script>
 import { getWilayah } from "@/mixins/getWilayah.js";
 import { getSekolah } from "@/mixins/getSekolah.js";
+import sekolah from "@/components/data/Data.vue";
+import loading from "@/components/loading/Loading.vue";
 export default {
   name: "Sekolah",
   mixins: [getWilayah, getSekolah],
@@ -50,6 +97,10 @@ export default {
     return {
       bentuk: ["SD", "SMP", "SMA", "SMK"]
     };
+  },
+  components: {
+    loading,
+    sekolah
   }
 };
 </script>
