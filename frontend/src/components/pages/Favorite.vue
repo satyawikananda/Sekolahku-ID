c42f22b1e7c3afb9a837fbcc7cc4a6fcf20bc162
Satya Wikananda <satyawikananda456@gmail.com>
2020-06-07 18:45:39 +0800

    :zap: Change pin page

--- frontend/src/components/pages/Favorite.vue
+++ frontend/src/components/pages/Favorite.vue
@@ -1,6 +1,14 @@
 <template>
-  <v-container>
-    <h1>Hello world</h1>
+  <v-container grid-list-xl>
+    <v-layout row justify-space-between>
+      <v-flex md4>
+        <p
+          class="red--text mx text--darken-2 text-center pb-2 title font-weight-reguler"
+        >
+          Dalam pembuatan
+        </p>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 <script>
