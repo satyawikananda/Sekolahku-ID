2cc2082f360989591da4a4fa42efb12b8f358ede
Satya Wikananda <satyawikananda456@gmail.com>
2020-06-07 23:15:43 +0800

    :sparkles: Say hi to i18n

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import router from "./routes/Router.js";
 import "./registerServiceWorker";
 import Axios from "axios";
 import vuetify from "./plugins/vuetify";
+import i18n from "./i18n";
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = Axios;
@@ -11,5 +12,6 @@ Vue.prototype.$http = Axios;
 new Vue({
   vuetify,
   router,
-  render: (h) => h(App),
+  i18n,
+  render: h => h(App)
 }).$mount("#app");
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   lintOnSave: false,
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: true
+    }
+  }
 };
