066fb2c3e53adac1700b4be240a98302eb37cac9
Satya Wikananda <satyawikananda456@gmail.com>
2020-06-25 12:15:57 +0800

    Deploying folder dist to gh-pages on 6/25/2020, 12:15:49 PM

---
+++ precache-manifest.25b3c0ad59c7bc92628f6b55d9ca02cb.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64fe3c0badb912b8b15b",
+    "url": "/css/app.96577ac5.css"
+  },
+  {
+    "revision": "d95471afde041d772fc7",
+    "url": "/css/chunk-2d3fa138.45fe1549.css"
+  },
+  {
+    "revision": "a63bf3d7ca1ae27da7c4",
+    "url": "/css/chunk-33771f30.c95c45f0.css"
+  },
+  {
+    "revision": "88655a37ff6ea7825e2f",
+    "url": "/css/chunk-3ea84b0f.85e964b6.css"
+  },
+  {
+    "revision": "626fdc40cfb11e3b50eb",
+    "url": "/css/chunk-582e24c9.27b892c3.css"
+  },
+  {
+    "revision": "90fc53acc4d18a9012da",
+    "url": "/css/chunk-6ba1b0f9.b5464fbc.css"
+  },
+  {
+    "revision": "f06883af17c23c0162b8",
+    "url": "/css/chunk-cb6fbbae.85e964b6.css"
+  },
+  {
+    "revision": "6089bf261b5fb3e93af3",
+    "url": "/css/chunk-vendors.9149db6a.css"
+  },
+  {
+    "revision": "bfabfac0919988fe6ee33c95dad2c6ec",
+    "url": "/img/undraw_back_to_school_inwc.bfabfac0.svg"
+  },
+  {
+    "revision": "c332050c29ba9c2335916e61860f1a01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64fe3c0badb912b8b15b",
+    "url": "/js/app.fe3cb126.js"
+  },
+  {
+    "revision": "d95471afde041d772fc7",
+    "url": "/js/chunk-2d3fa138.c37ea8d4.js"
+  },
+  {
+    "revision": "a63bf3d7ca1ae27da7c4",
+    "url": "/js/chunk-33771f30.d79aef79.js"
+  },
+  {
+    "revision": "88655a37ff6ea7825e2f",
+    "url": "/js/chunk-3ea84b0f.7faa91ea.js"
+  },
+  {
+    "revision": "626fdc40cfb11e3b50eb",
+    "url": "/js/chunk-582e24c9.2359a624.js"
+  },
+  {
+    "revision": "90fc53acc4d18a9012da",
+    "url": "/js/chunk-6ba1b0f9.869502a8.js"
+  },
+  {
+    "revision": "f06883af17c23c0162b8",
+    "url": "/js/chunk-cb6fbbae.d851fa8f.js"
+  },
+  {
+    "revision": "6089bf261b5fb3e93af3",
+    "url": "/js/chunk-vendors.4234ea66.js"
+  },
+  {
+    "revision": "529cb5765936a9c1467313022ddbd7bc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
